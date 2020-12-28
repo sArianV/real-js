862ca9220ebc64e53c90fa7e8bdb03615e0aa2c9
sArianV <yoarian94@gmail.com>
2020-12-28 02:55:09 -0300

    recibo parametros y los muestro

--- backend/controllers/article.js
+++ backend/controllers/article.js
@@ -6,6 +6,17 @@ var controller = {
             name: 'nico' ,
             status: 'gato'
         }));
+    },
+    save: (req, res) => {
+       var params = req.body;
+        var newArticle = {
+            id: params.id,
+            name: params.name,
+            description: params.description, 
+            avg: 999999,
+            image: "../src/products/"+ params.id 
+        }
+        return(res.status(200).send(newArticle));
     }
 }
 
--- backend/routes/article.js
+++ backend/routes/article.js
@@ -6,5 +6,6 @@ var ArticleController = require ('../controllers/article');
 var router = express.Router();
 
 router.get('/testNico', ArticleController.test);
+router.post('/test-save', ArticleController.save);
 
 module.exports = router;
