eb58ecde606852433ef8a8de37f728c915ee3d13
Valentin Tobares <valetobares11@gmail.com>
2020-12-28 15:29:24 -0300

    save in DB

--- backend/controllers/article.js
+++ backend/controllers/article.js
@@ -13,26 +13,47 @@ var controller = {
     save: (req, res) => {
        var params = req.body;
        try{
-        var validate_id = !validator.isEmpty(params.id);
         var validate_name = !validator.isEmpty(params.name);
         var validate_description = !validator.isEmpty(params.description);
 
-       }catch(error){
+       }catch(err){
             return res.status(200).send({
+                status: 'error',
                 message:'Faltan datos por enviar!'
             })
        }
-       if(validate_id && validate_name && validate_description){
-        var newArticle = {
+       if( validate_name && validate_description){
+       var article = new Article();
+       article.name = params.name;
+       article.description = params.description;
+       article.image = `../src/products/${params.id}`;
+       
+       article.save((err, articleStored)=> {
+        if(err || !articleStored){
+            return res.status(404).send({
+                status:'error',
+                message:'El articulo no se a guardado'
+            });
+        }
+        return res.status(200).send({
+            status: 'success',
+            article:articleStored 
+        }); 
+       });
+
+      
+       
+        /* var newArticle = {
             id: params.id,
             name: params.name,
             description: params.description, 
             avg: 999999,
             image: "../src/products/"+ params.id 
         }
-        return(res.status(200).send(newArticle));
+        return(res.status(200).send(newArticle));*/
        }else{
         return res.status(200).send({
+            status: 'error',
             message:'Los datos no son correctos'
         })
        }
--- backend/models/article.js
+++ backend/models/article.js
@@ -4,7 +4,6 @@ var Schema = mongoose.Schema;
 
 //avg TODO
 var ArticleSchema = Schema({
-    id: String,
     name: String,
     description: String, 
     avg: Number,
