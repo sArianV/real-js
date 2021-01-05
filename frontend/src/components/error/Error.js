0ae466b16f4f2c976b3f2ebe012a16c263742b52
Valentin Tobares <valetobares11@gmail.com>
2021-01-05 20:37:45 -0300

    imagen de error

--- frontend/src/components/error/Error.js
+++ frontend/src/components/error/Error.js
@@ -1,13 +1,18 @@
 import React, {Component} from 'react';
+import './error.css';
+import imga from "./../../assets/images/error_pag.gif";
 
 const Error = () =>{
 
     return(
-        <div className="msgerror" >
-            <h2>
-                Pag no encontrada
-                <p>La pag a la que intentas acceder no existe en la web</p>
-            </h2>    
+        <div className="error" >
+          <div className="hh">
+          <h2>Pagina no encontrada</h2> 
+            <h2>La pagina a la que intentas acceder no existe en la web</h2>
+          </div>  
+          <div className="imgError">
+             <img src= {imga}/>
+          </div>
         </div>
     );
 }
