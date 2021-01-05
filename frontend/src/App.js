cec4b8ab1100b1159795a4485fe366d17a65da05
Valentin Tobares <valetobares11@gmail.com>
2021-01-05 00:46:51 -0300

    rutas

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,13 @@ import '@fontsource/roboto';
 
 //import Material ui
 import Grid from '@material-ui/core/Grid';
+import { Route, BrowserRouter, Switch } from 'react-router-dom'
 
 
 //import componentes
 import Navbar from './components/navbar/Navbar';
 import Login from './components/login/Login';
 import CreateAccount from './components/create_account/CreateAccount';
-
 import BodyContent from './components/bodyContent/bodyContent'
 
 require('pretty-error').start();
@@ -20,12 +20,11 @@ function App() {
     <div className="container">
       <div className="App">
         <Grid container xs={12} spacing={0}>
-          <CreateAccount />
-          <Login />
-          
-          <Navbar />
-          <BodyContent />
-          
+          <Switch>
+            <Route exact path='/' component={Navbar}/>
+            <Route path='/login' component={Login}/>
+            <Route path='/createaccount' component={CreateAccount} />
+          </Switch>
         </Grid>
       </div>
     </div>
