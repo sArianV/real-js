c6b23a1f54cea2ccd883ec43cc821195fb6d8de1
Valentin Tobares <valetobares11@gmail.com>
2021-01-05 20:53:26 -0300

    jdhfsdf

--- frontend/src/components/error/Error.js
+++ frontend/src/components/error/Error.js
@@ -7,12 +7,10 @@ const Error = () =>{
     return(
         <div className="error" >
           <div className="hh">
-          <h2>Pagina no encontrada</h2> 
+            <h2>Pagina no encontrada</h2> 
             <h2>La pagina a la que intentas acceder no existe en la web</h2>
           </div>  
-          <div className="imgError">
-             <img src= {imga}/>
-          </div>
+         
         </div>
     );
 }
