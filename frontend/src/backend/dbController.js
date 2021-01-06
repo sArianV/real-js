4f9d872e161279541fd98bd7416847e62025d045
Nicolas D'Alessandro <nicolasdalessandro2@gmail.com>
2021-01-06 17:57:36 -0300

    init sequelize

--- frontend/src/backend/dbController.js
+++ frontend/src/backend/dbController.js
@@ -1,33 +1,8 @@
-var sqlite3 = require('sqlite3').verbose();
-var fs = require('fs');
+const { Sequelize } = require('sequelize');
 
-let db = new sqlite3.Database('./db/basedato.db', (err) => {
-    if (err) {
-        console.error(err.message);
-    }
-    console.log('Connected to the chinook database.');
-});
+const sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: './db/basedatos.sqlite'
+})
 
-const dataSql = fs.readFileSync('./db/init.sql').toString();
-
-const dataArr = dataSql.toString().split(';');
-db.serialize(() => {
-  db.run('PRAGMA foreign_keys=OFF;');
-  db.run('BEGIN TRANSACTION;');
-  dataArr.forEach((query) => {
-    if(query) {
-      query = query.trim() + ';';
-      db.run(query, (err) => {
-         if(err) throw err;
-      });
-    }
-  });
-  db.run('COMMIT;');
-});
-
-db.close((err) => {
-    if (err) {
-        return console.error(err.message);
-    }
-    console.log('Close the database connection.');
-});
+console.log(sequelize)
