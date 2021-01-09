cc0fd41f7e977e94733abe8657f62d16d4954403
Valentin Tobares <valetobares11@gmail.com>
2021-01-08 23:10:37 -0300

    .\extra-setup.js

--- frontend/src/backend/extra-setup.js
+++ frontend/src/backend/extra-setup.js
@@ -1,8 +1,15 @@
-function applyExtraSetup(sequelize) {
-	//const { instrument, orchestra } = sequelize.models;
 
-	//orchestra.hasMany(instrument);
-	//instrument.belongsTo(orchestra);
+function applyExtraSetup(sequelize) {
+	const { customer, product, orderItem, supplier,order } = sequelize.models;
+	product.hasMany(supplier);
+	supplier.belongs(product);
+	order.hasMany(customer);
+	customer.belongs(order);
+	orderItem.hasMany(order);
+	order.belongs(orderItem);
+	orderItem.hasMany(product);
+	product.belongs(orderItem);
+
 }
 //https://github.com/sequelize/express-example/tree/master/express-main-example/sequelize
 module.exports = { applyExtraSetup };
