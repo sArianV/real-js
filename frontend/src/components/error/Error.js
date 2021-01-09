fb1b4ba1e6a10ec4264c59461ecd6c7da47aebb7
agus-droid <agustin9845@gmail.com>
2021-01-09 15:33:39 -0300

    Error

--- frontend/src/components/error/Error.js
+++ frontend/src/components/error/Error.js
@@ -6,13 +6,11 @@ const Error = () => {
   return (
     
       <div className="error" >
-        <div className="hh">
+        <div className="hh"></div>
           <div id="errorMsg">
             <h2>Pagina no encontrada</h2>
             <h3>La pagina a la que intentas acceder no existe &#128514;</h3>
           </div>
-
-        </div>
 
       </div>
   );
