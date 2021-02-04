b1d67560ec664a4d648b98fcbb5a2a139dc293ee
sArianV <yoarian94@gmail.com>
2021-02-04 13:58:44 -0300

    caja

--- src/components/routesComponents/caja/caja.js
+++ src/components/routesComponents/caja/caja.js
@@ -3,36 +3,60 @@ import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
 import sendAsync from "../../../message-control/renderer";
 import Autocomplete from '@material-ui/lab/Autocomplete';
-
-
-import './style.css';
+import Style from "./caja.module.css";
 
 const code = require("../../../message-control/MessajeConst");
 
 class Caja extends Component {
-    
+
     constructor(props) {
         super(props);
-        this.state = { productos: []};
-      }
-    
-      componentDidMount(){
+        this.state = { productos: [] };
+    }
+
+    componentDidMount() {
         const response = sendAsync({
-          code: code.GET_PRODUCTS,
+            code: code.GET_PRODUCTS,
         });
-    
+
         response.then((product) => {
-        
-          product.map((x)=> x.ProductName =  x.ProductName.charAt(0).toUpperCase()+ x.ProductName.slice(1))
-          this.setState({productos: product})   
-          console.log(product)   
+
+            product.map((x) => x.ProductName = x.ProductName.charAt(0).toUpperCase() + x.ProductName.slice(1))
+            this.setState({ productos: product })
+            console.log(product)
         });
-      }
- 
+    }
+
     render() {
 
 
         return (
+            <div className={Style.cajaBody}>
+                <div className={Style.listItems}>aca va el componente de la lista de compras </div>
+                <div className={Style.in}>
+                    <div className={Style.imputBox}> componente donde se eligen los items
+                        <Autocomplete
+                            id="grouped-products"
+                            options={this.state.productos.sort((a, b) => a.ProductName.charAt(0) - b.ProductName.charAt(0))}
+                            groupBy={(option) => option.ProductName.charAt(0)}
+                            getOptionLabel={(option) => option.ProductName}
+                            style={{ width: 300 }}
+                            renderInput={(params) => <TextField {...params} label="Productos" variant="outlined" />}
+                        />
+                        <Autocomplete
+                            id="grouped-products"
+                            options={this.state.productos.sort((a, b) => a.barcode_1 - b.barcode_1)}
+                            getOptionLabel={(option) => option.barcode_1}
+                            style={{ width: 300 }}
+                            renderInput={(params) => <TextField {...params} label="Codigo" variant="outlined" />}
+                        />
+
+                    </div>
+                    <div className={Style.checkOutBox}> componente del total y el medio de pago</div>
+                </div>
+            </div>
+
+            /*
             <Grid container spacing={0}>
                 <Autocomplete
                     id="grouped-products"
@@ -59,7 +83,7 @@ class Caja extends Component {
                         <input type="button" value="Restar" onClick={this.restar} />
                     </p>
                 </div>
-            </Grid>
+        </Grid>*/
         )
     }
 }
